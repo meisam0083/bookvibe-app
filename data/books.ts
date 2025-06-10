d2b6a8640c61bf984b93f99daebf11df3e615b4b
meisam0083 <meisam0083@yahoo.com>
2025-06-10 22:31:42 +0330

    Update books.ts

--- data/books.ts
+++ data/books.ts
@@ -1,1 +1,27 @@
-1
+// file: data/books.ts
+export const books = [
+  {
+    id: 1,
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    imageUrl: 'https://m.media-amazon.com/images/I/81wgcld4wxL._AC_UY218_.jpg',
+  },
+  {
+    id: 2,
+    title: 'The Psychology of Money',
+    author: 'Morgan Housel',
+    imageUrl: 'https://m.media-amazon.com/images/I/61-hMfd7NGL._AC_UY218_.jpg',
+  },
+  {
+    id: 3,
+    title: 'Ikigai',
+    author: 'Héctor García',
+    imageUrl: 'https://m.media-amazon.com/images/I/71t4GuxLCuL._AC_UY218_.jpg',
+  },
+  {
+    id: 4,
+    title: 'Deep Work',
+    author: 'Cal Newport',
+    imageUrl: 'https://m.media-amazon.com/images/I/71r4p6aR2-L._AC_UY218_.jpg',
+  },
+];
